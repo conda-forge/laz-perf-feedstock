8215b2b78840a8d3e32d8247088ccf66fd54f482
Howard Butler <hobu.inc@gmail.com>
2021-11-02 09:36:03 -0500

    try to do arm

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,13 @@
 
 set -ex
 
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
+    (
+        unset LDFLAGS
+        unset CXXFLAGS
+        export CXX=${CXX_FOR_BUILD}
+fi
+
 cmake -G "Unix Makefiles" \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
